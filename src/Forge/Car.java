77310177eca94b6a295023f9fff5b723b7ff3c79
MARVINAKI <kosakbig@gmial.com>
2022-11-15 17:36:33 +0500

    Exercise 3

--- src/Forge/Car.java
+++ src/Forge/Car.java
@@ -1,5 +1,6 @@
 package Forge;
 
+
 public class Car {
     private String brand;
     private String model;
@@ -9,17 +10,18 @@ public class Car {
     private String productionCountry;
 
     public Car(String brand, String model, int productionYear, String productionCountry, String color, Double engineVolume) {
-        this.brand = brand.trim();
-        this.model = model.trim();
-        this.productionYear = Math.abs(productionYear);
-        this.productionCountry = productionCountry.trim();
-        this.color = color.trim();
-        this.engineVolume = Math.abs(engineVolume);
+        this.brand = brand == null || brand.trim().isEmpty() ? "default" : brand.trim();
+        this.model = model == null || model.trim().isEmpty() ? "default" : model.trim();
+        this.productionYear = productionYear <= 0 ? 2000 : productionYear;
+        this.productionCountry = productionCountry == null || productionCountry.trim().isEmpty() ? "default" : productionCountry.trim();
+        this.color = color == null || color.trim().isEmpty() ? "белый" : color;
+        this.engineVolume = engineVolume <= 0 ? 1.5 : engineVolume;
     }
+
 
     @Override
     public String toString() {
-        return "Car{" +
+        return "Аааавтомобиль {" +
                 "Марка='" + brand + '\'' +
                 ", модель='" + model + '\'' +
                 ", объём двигателя (литров)=" + engineVolume +
--- src/Forge/Main.java
+++ src/Forge/Main.java
@@ -4,13 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Human human1 = new Human("Максим", "Минск", 1988, "Бренд-менеджер");
+        Human human1 = new Human(null, "Минск", "Бренд-менеджер");
         Human human2 = new Human("Аня", "Москва", 1993, "Методист образовательных программ");
         Human human3 = new Human("Катя", "Калининград", 1992, "Продакт-менеджер");
         Human human4 = new Human("Артём", "Москва", 1995, "Директор по развитию бизнеса");
 
 
-        Car car1 = new Car("Lada", "Granta", 2015, "РФ", "жёлтый", 1.7);
+        Car car1 = new Car("Lada", "", 2015, "РФ", "", 1.7);
         Car car2 = new Car("Audi", "A8", 2020, "Германия", "чёрный", 3.0);
         Car car3 = new Car("BMW", "Z8", 2021, "Германия", "чёрный", 3.0);
         Car car4 = new Car("Kia", "Sportage (4 gen)", 2018, "Южная Корея", "красный", 2.4);
@@ -18,8 +18,7 @@ public class Main {
 
 
         human1.greetings();
-        System.out.println(human1);
-
-        System.out.println(car4);
+        System.out.println(car1);
     }
+
 }
