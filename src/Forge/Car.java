9ffe6cb4de060e6d7c865e1f25a6e9cb09fbe0a5
MARVINAKI <kosakbig@gmial.com>
2022-11-18 22:33:53 +0500

    Ex.1-2

--- src/Forge/Car.java
+++ src/Forge/Car.java
@@ -1,6 +1,8 @@
 package Forge;
 
 
+import java.util.Objects;
+
 public class Car {
     private String brand;
     private String model;
@@ -29,5 +31,18 @@ public class Car {
                 ", год производства=" + productionYear +
                 ", страна сборки='" + productionCountry + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return productionYear == car.productionYear && brand.equals(car.brand) && model.equals(car.model) && engineVolume.equals(car.engineVolume) && color.equals(car.color) && productionCountry.equals(car.productionCountry);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brand, model, engineVolume, color, productionYear, productionCountry);
     }
 }
--- src/Forge/Human.java
+++ src/Forge/Human.java
@@ -1,10 +1,13 @@
 package Forge;
 
+import java.util.Objects;
+
 public class Human {
     private int yearOfBirth;
-    private String name;
+    public String name;
     private String town;
-    private String jobTitle;
+    public String jobTitle;
+    private static final String MESSAGE = "Информация не указана";
 
     public Human(String name, String town, String jobTitle) {
         this(name, town, 0, jobTitle);
@@ -12,17 +15,43 @@ public class Human {
 
 
     public Human(String name, String town, int yearOfBirth, String jobTitle) {
-        this.name = name == null ? "\'Информация не указана\'" : name.trim();
-        this.town = town == null || town.trim().isEmpty() ? "\'Информация не указана\'" : town.trim();
+        this.name = name == null || name.trim().isEmpty() ? MESSAGE : name.trim();
+        this.town = town == null || town.trim().isEmpty() ? MESSAGE : town.trim();
         this.yearOfBirth = Math.abs(yearOfBirth);
-        this.jobTitle = jobTitle == null || jobTitle.trim().isEmpty() ? "\'Информация не указана\'" : jobTitle.trim();
+        this.jobTitle = jobTitle == null || jobTitle.trim().isEmpty() ? MESSAGE : jobTitle.trim();
     }
 
     public void greetings() {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города "
-                + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности "
-                + jobTitle + ". Будем знакомы!");
+        if (name.equals(MESSAGE) || town.equals(MESSAGE) || jobTitle.equals(MESSAGE) || yearOfBirth == 0) {
+            System.out.println("Для приветсвия недостаточно информации");
+        } else {
+            System.out.println("Привет! Меня зовут " + name + ". Я из города "
+                    + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности "
+                    + jobTitle + ". Будем знакомы!");
+        }
     }
+
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth() {
+        this.yearOfBirth = 0;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth <= 0 ? 0 : yearOfBirth;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town == null || town.trim().isEmpty() ? MESSAGE : town.trim();
+    }
+
 
     @Override
     public String toString() {
@@ -30,6 +59,21 @@ public class Human {
                 " Имя=" + name +
                 ", город='" + town + '\'' +
                 ", год рождения='" + yearOfBirth + '\'' +
+                ", должность='" + jobTitle + '\'' +
                 '}';
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Human human = (Human) o;
+        return yearOfBirth == human.yearOfBirth && name.equals(human.name) && town.equals(human.town) && jobTitle.equals(human.jobTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(yearOfBirth, name, town, jobTitle);
     }
 }
